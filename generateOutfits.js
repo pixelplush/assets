9647f405c8ad0b94d8924d360d6b89481dceaa96
instafluff <instaflufftv@gmail.com>
2021-11-21 09:18:42 -0800

    Pull Outfit Catalog from GSheet

--- generateOutfits.js
+++ generateOutfits.js
@@ -1,13 +1,26 @@
+require( "dotenv" ).config();
+
 const fs = require( "fs" );
+const ComfySheets = require( "comfysheets" );
 
-// Outfit Items
-const types = [ "body", "equipment", "outfits", "accessories" ];
-let outfitItems = [];
-for( const type of types ) {
-    const items = JSON.parse( fs.readFileSync( `${type}.json` ).toString() );
-    outfitItems.push( ...items );
-}
-// console.log( outfitItems );
+const SHEET_ID = "1OgJA5tPAzTkGg3pjpctYMZNET6cwEBs8HuL41Lq-UyA";
 
-fs.writeFileSync( `catalog_outfits.json`, JSON.stringify( outfitItems, null, "\t" ) );
-console.log( `Saved catalog_outfits.json` );
+( async () => {
+  let outfitItems = await ComfySheets.Read( SHEET_ID, 'Outfits', {} );
+  console.log( outfitItems );
+  fs.writeFileSync( `catalog_outfits.json`, JSON.stringify( outfitItems, null, "\t" ) );
+  console.log( `Saved catalog_outfits.json` );
+})();
+
+//
+// // Outfit Items
+// const types = [ "body", "equipment", "outfits", "accessories" ];
+// let outfitItems = [];
+// for( const type of types ) {
+//     const items = JSON.parse( fs.readFileSync( `${type}.json` ).toString() );
+//     outfitItems.push( ...items );
+// }
+// // console.log( outfitItems );
+//
+// fs.writeFileSync( `catalog_outfits.json`, JSON.stringify( outfitItems, null, "\t" ) );
+// console.log( `Saved catalog_outfits.json` );
